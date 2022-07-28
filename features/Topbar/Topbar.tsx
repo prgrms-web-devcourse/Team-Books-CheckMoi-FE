e452a36382968023bfc260ffb09a2a9b96de9b02
feel0321 <dishate@naver.com>
2022-07-28 17:11:17 +0900

    feat: Topbar 검색 폼, 로그인 버튼 추가

--- features/Topbar/Topbar.tsx
+++ features/Topbar/Topbar.tsx
@@ -1,17 +1,30 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
-import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
 import * as S from "./style";
 
 export const Topbar = () => {
   return (
     <AppBar position="fixed" sx={{ backgroundColor: "green" }}>
       <Toolbar>
-        <AutoStoriesIcon />
+        <S.LogoIcon />
         <S.LogoText variant="h6" noWrap>
           책모이
         </S.LogoText>
+        <S.SearchWrapper>
+          <S.Search>
+            <S.SearchIconWrapper>
+              <SearchIcon />
+            </S.SearchIconWrapper>
+            <S.StyledInputBase
+              placeholder="책을 검색해주세요"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </S.Search>
+        </S.SearchWrapper>
+        <Button variant="contained">로그인</Button>
       </Toolbar>
     </AppBar>
   );
--- features/Topbar/style.tsx
+++ features/Topbar/style.tsx
@@ -1,13 +1,74 @@
-import { styled } from "@mui/material/styles";
-import { Typography } from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
 
-export const LogoText = styled(Typography)`
-  margin-left: 0.5rem;
-  margin-right: 0.5rem;
-  display: flex;
-  font-family: monospace;
-  font-weight: 700;
-  letter-spacing: 0.3rem;
-  color: inherit;
-  text-decoration: none;
-`;
+export const LogoIcon = styled(AutoStoriesIcon)(() => ({
+  marginRight: "0.5rem",
+}));
+
+export const LogoText = styled(Typography)(({ theme }) => ({
+  marginRight: "0.5rem",
+  display: "flex",
+  fontFamily: "monospace",
+  fontWeight: "700",
+  letterSpacing: "0.3rem",
+  color: "inherit",
+  textDecoration: "none",
+
+  [theme.breakpoints.down("sm")]: {
+    display: "none",
+  },
+}));
+
+export const SearchWrapper = styled("div")(() => ({
+  flexGrow: 1,
+  display: "flex",
+  justifyContent: "center",
+}));
+
+export const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  marginLeft: 0,
+  width: "100%",
+
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+export const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+export const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+
+    [theme.breakpoints.up("sm")]: {
+      width: "20rem",
+
+      "&:focus": {
+        width: "30rem",
+      },
+    },
+  },
+}));
