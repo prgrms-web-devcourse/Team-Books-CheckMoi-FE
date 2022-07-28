9a31c6312acb5d5e29211898a35be363546ad853
devBuzz142 <devbuzz142@gmail.com>
2022-07-28 18:48:37 +0900

    feat: StudyCard 컴포넌트 초기 구현 - 이미지 크기 fixed 문제 해결 - text ellipsis 문제 해결 필요 - defaultValue 문제 해결 필요

--- components/StudyCard/StudyCard.tsx
+++ components/StudyCard/StudyCard.tsx
@@ -1,10 +1,53 @@
-import Card from "@mui/material/Card";
+import Image from "next/image";
 import * as S from "./style";
 
-interface TestProps {
-  string: string;
+interface StudyCardProps {
+  size: number;
+  src: string;
+  title: string;
+  gatherStartDate: string;
+  gatherEndDate: string;
+  studyStartDate: string;
+  studyEndDate: string;
+  maxParticipant: number;
+  currentParticipant: number;
 }
 
-export const StudyCard = ({ string }: TestProps) => {
-  return <Card />;
+// TODO defaultValue 수정
+export const StudyCard = ({
+  size = 128,
+  src = "https://picsum.photos/200",
+  title = "스터디 제목",
+  gatherStartDate = new Date(2022, 8, 31).toString(),
+  gatherEndDate = new Date(2022, 8, 31).toString(),
+  studyStartDate = new Date(2022, 8, 31).toString(),
+  studyEndDate = new Date(2022, 8, 31).toString(),
+  maxParticipant = 16,
+  currentParticipant = 0,
+}: StudyCardProps) => {
+  return (
+    <S.StudyCard sx={{ textOverflow: "ellipsis", overflow: "hidden" }}>
+      <S.ImageWrapper>
+        <Image
+          layout="fixed"
+          width={size}
+          height={size * 1.5}
+          src={src}
+          style={{ flexShrink: 0 }}
+        />
+      </S.ImageWrapper>
+      <S.StudyInfoConatiner>
+        <S.ResponsiveText>스터디 제목 : {title}</S.ResponsiveText>
+        <S.ResponsiveText>
+          모집 기간 : {gatherStartDate} - {gatherEndDate}
+        </S.ResponsiveText>
+        <S.ResponsiveText>
+          모집 인원 : {currentParticipant}/{maxParticipant}
+        </S.ResponsiveText>
+        <S.ResponsiveText>
+          진행 기간 : {studyStartDate} - {studyEndDate}
+        </S.ResponsiveText>
+      </S.StudyInfoConatiner>
+    </S.StudyCard>
+  );
 };
--- components/StudyCard/style.tsx
+++ components/StudyCard/style.tsx
@@ -1,9 +1,39 @@
 import styled from "@emotion/styled";
+import { Card } from "@mui/material";
 
-interface StyledTestProps {
-  string: string;
-}
+export const StudyCard = styled(Card)`
+  display: flex;
 
-export const StyledTest = styled.div<StyledTestProps>`
-  background-color: ${({ string }) => (string ? "black" : "yellow")};
+  width: 100%;
+
+  padding: 1rem;
+`;
+
+export const ImageWrapper = styled.div`
+  flex-shrink: 0;
+`;
+
+// TODO ellipsis 적용 안됨
+export const StudyInfoConatiner = styled.div`
+  display: block;
+
+  padding: 1rem;
+
+  flex-basis: 200px;
+
+  &:first-child {
+    font-size: 1.33rem;
+  }
+`;
+
+export const ResponsiveText = styled.div`
+  width: 100%;
+
+  display: block;
+
+  padding: 0.5rem;
+
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
