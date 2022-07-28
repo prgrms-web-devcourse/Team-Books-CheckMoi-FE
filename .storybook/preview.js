06ab9349aeb94c9d4926fe219a0da9aba7367fe7
devBuzz142 <devbuzz142@gmail.com>
2022-07-28 18:32:23 +0900

    fix: nextjs - storybook 이미지 버그 해결

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,12 @@
+import * as NextImage from "next/image";
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +15,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
