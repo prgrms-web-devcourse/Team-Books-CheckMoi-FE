ea3e991422a1dd4577893831c20bd7335c3c3137
feel0321 <dishate@naver.com>
2022-07-28 16:27:41 +0900

    feat: Topbar 추가

---
+++ features/Topbar/Topbar.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import AutoStoriesIcon from "@mui/icons-material/AutoStories";
+import * as S from "./style";
+
+export const Topbar = () => {
+  return (
+    <AppBar position="fixed" sx={{ backgroundColor: "green" }}>
+      <Toolbar>
+        <AutoStoriesIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+        <S.LogoText variant="h6" noWrap>
+          책모이
+        </S.LogoText>
+      </Toolbar>
+    </AppBar>
+  );
+};
---
+++ features/Topbar/style.tsx
@@ -0,0 +1,12 @@
+import { styled } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+
+export const LogoText = styled(Typography)`
+  margin-right: 1rem;
+  display: flex;
+  font-family: monospace;
+  font-weight: 700;
+  letter-spacing: 0.3rem;
+  color: inherit;
+  text-decoration: none;
+`;
