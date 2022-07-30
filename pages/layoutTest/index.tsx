71bd2decb9c4749ec33c2f25b4a53838ec6d81f4
feel0321 <dishate@naver.com>
2022-07-30 17:38:08 +0900

    feat: layoutTest 페이지에 검색 결과 페이지네이션 추가
    docs: TODO 북카드 가져오기

--- pages/layoutTest/index.tsx
+++ pages/layoutTest/index.tsx
@@ -1,5 +1,7 @@
-import { useRouter } from "next/router";
 import Image from "next/future/image";
+import { useState, useEffect } from "react";
+import type { ChangeEvent } from "react";
+import Pagination from "@mui/material/Pagination";
 
 interface DummyProps {
   word: string | undefined | string[];
@@ -26,14 +28,57 @@ const DummyBook = ({ word }: DummyProps) => {
 };
 
 const LayoutTestPage = () => {
-  const router = useRouter();
-  const { word } = router.query;
   // TODO 더미 데이터 만들어서 페이지네이션 만들기
+  const displayBookCount = 8;
+
+  const dummyArray = Array.from(new Array(100), (x, i) => `${i + 1}`);
+  const lastPage = Math.ceil(dummyArray.length / displayBookCount);
+  const [page, setPage] = useState(1);
+  const [pageData, setPageData] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (page === lastPage)
+      setPageData(dummyArray.slice(displayBookCount * (page - 1)));
+    else
+      setPageData(
+        dummyArray.slice(
+          displayBookCount * (page - 1),
+          displayBookCount * (page - 1) + displayBookCount
+        )
+      );
+  }, [page]);
+
+  const handlePage = (e: ChangeEvent<unknown> | null, inputPage: number) => {
+    setPage(inputPage);
+  };
+
   return (
-    <div>
-      {word}
-      <DummyBook word={word} />
-    </div>
+    <>
+      <div>
+        <ul
+          style={{
+            listStyle: "none",
+            display: "grid",
+            gridTemplateRows: "1fr 1fr",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+          }}
+        >
+          {pageData.map((data) => (
+            <li key={Math.random()}>
+              <DummyBook word={data} />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Pagination
+        count={Math.ceil(dummyArray.length / 8)}
+        variant="outlined"
+        shape="rounded"
+        color="primary"
+        page={page}
+        onChange={handlePage}
+      />
+    </>
   );
 };
 
