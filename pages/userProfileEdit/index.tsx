98e4eb4d075a654b6b54f2c82fa40ac4c450b5d6
parkinhwa <inhwa6548@naver.com>
2022-08-01 22:03:02 +0900

    feat: UserProfileEditPage 추가

---
+++ pages/userProfileEdit/index.tsx
@@ -0,0 +1,49 @@
+import { Badge } from "@mui/material";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import * as S from "../../styles/UserProfileEditStyle";
+
+interface UserProfileProps {
+  name: string;
+  email: string;
+  profileImageUrl: string;
+}
+
+const UserProfileEditPage = ({
+  name = "사용자 이름",
+  email = "1234@naver.com",
+  profileImageUrl = "https://picsum.photos/200",
+}: UserProfileProps) => {
+  return (
+    <S.Container>
+      <S.UserProfileImage>
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          badgeContent={
+            <S.SmallAvatar>
+              <CameraAltIcon />
+            </S.SmallAvatar>
+          }
+        >
+          <S.StyledAvatar src={profileImageUrl} />
+        </Badge>
+      </S.UserProfileImage>
+      <S.StyledTextField
+        id="full-width-text-field"
+        label="이름"
+        defaultValue={name}
+      />
+      <S.StyledTextField
+        id="full-width-text-field"
+        label="이메일"
+        defaultValue={email}
+      />
+      <S.ButtonContainer>
+        <S.StyledButton variant="contained">수정하기</S.StyledButton>
+        <S.StyledButton variant="contained">뒤로가기</S.StyledButton>
+      </S.ButtonContainer>
+    </S.Container>
+  );
+};
+
+export default UserProfileEditPage;
