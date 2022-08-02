35a51df3786cc49669aa7b0598fc5865455c3b61
parkinhwa <inhwa6548@naver.com>
2022-08-02 17:13:45 +0900

    feat: 컴포넌트 export 파일 생성

---
+++ components/index.ts
@@ -0,0 +1,5 @@
+export { BookCard } from "./BookCard";
+export { BookDetail } from "./BookDetailCard";
+export { StudyCard } from "./StudyCard";
+export { StudyContent } from "./StudyContent";
+export { TabPanel } from "./TabPanel";
