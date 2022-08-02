fb452967fd2705bd7d24acb6183d42a8e2d032a1
parkinhwa <inhwa6548@naver.com>
2022-08-02 17:13:15 +0900

    feat: TabPanel 컴포넌트 추가

---
+++ components/TabPanel/TabPanel.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+export const TabPanel = ({ children, value, index }: TabPanelProps) => {
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
