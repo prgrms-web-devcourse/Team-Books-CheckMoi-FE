a7c2c16635451a532edecbc0e60e238d2da6bc1a
Minki Kim <alsrl0023@naver.com>
2022-08-04 00:59:06 +0900

    feat: 북 카드 컴포넌트 클릭 시 상세 페이지로 이동

--- components/BookCard/BookCard.tsx
+++ components/BookCard/BookCard.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 import * as S from "./style";
 
 interface BookCardProps {
+  id: number;
   src: string;
   title: string;
   size: number;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-export const BookCard = ({ src, title, size, onClick }: BookCardProps) => {
+export const BookCard = ({ id, src, title, size, onClick }: BookCardProps) => {
+  const handleClick = () => {
+    // console.log("id", id);
+  };
   return (
-    <S.BookCard onClick={onClick} size={size}>
+    <S.BookCard onClick={handleClick} size={size}>
       <S.ImageWrapper size={size}>
         <Image src={src} layout="fill" />
       </S.ImageWrapper>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import type { GetServerSideProps } from "next";
+import type { MouseEvent } from "react";
 import axios from "axios";
-import { BookCard } from "../components/BookCard";
+import { useRouter } from "next/router";
 import type { BookDataInterface } from "../types/testType";
+import { BookCard } from "../components/BookCard";
 import * as S from "../styles/MainPageStyle";
 
 interface ServerSidePropsType {
@@ -12,9 +14,18 @@ interface ServerSidePropsType {
 }
 
 const Home = ({ books }: ServerSidePropsType) => {
+  const router = useRouter();
   const { latestBooks, studyLatestBooks } = books;
-  const handleBookCardClick = () => {
+
+  const handleBookCardClick = (e: MouseEvent<HTMLElement>) => {
     // TODO 책 클릭 시 상세 페이지로 이동!
+    // TODO 클릭 시 id 값을 얻어와야 한다.
+    // TODO BookCard에 onClick 콜백을 전달하면, 내부에서 id값을 알 수 없기 때문에 사용 불가.
+    // TODO List에서 클릭시 이동하는 방향으로 수정해야 할듯?
+  };
+
+  const handleBookCardListClick = (key: number, e: MouseEvent<HTMLElement>) => {
+    router.push(`/book/${key}`);
   };
 
   return (
@@ -22,12 +33,16 @@ const Home = ({ books }: ServerSidePropsType) => {
       <S.StyledSpan>가장 최근 추가된 책</S.StyledSpan>
       <S.StyledUl>
         {latestBooks.map((book) => (
-          <S.StyledList key={book.id}>
+          <S.StyledList
+            key={book.id}
+            onClick={(e) => handleBookCardListClick(book.id, e)}
+          >
             <BookCard
+              id={book.id}
               src={book.image}
               title=""
               size={10}
-              onClick={handleBookCardClick}
+              onClick={() => {}}
             />
           </S.StyledList>
         ))}
@@ -36,12 +51,16 @@ const Home = ({ books }: ServerSidePropsType) => {
       <S.StyledSpan>가장 최근 스터디가 만들어진 책</S.StyledSpan>
       <S.StyledUl>
         {studyLatestBooks.map((book) => (
-          <S.StyledList key={book.id}>
+          <S.StyledList
+            key={book.id}
+            onClick={(e) => handleBookCardListClick(book.id, e)}
+          >
             <BookCard
+              id={book.id}
               src={book.image}
               title=""
               size={10}
-              onClick={handleBookCardClick}
+              onClick={() => {}}
             />
           </S.StyledList>
         ))}
