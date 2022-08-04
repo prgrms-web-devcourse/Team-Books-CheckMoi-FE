9e01753444a754b4d3b543f6cd2f3612a212dddb
Minki Kim <alsrl0023@naver.com>
2022-08-04 15:30:58 +0900

    feat: BookCard에 콜백함수 주입으로 변경 docs: 주석 제거

--- components/BookCard/BookCard.tsx
+++ components/BookCard/BookCard.tsx
@@ -3,19 +3,15 @@ import Image from "next/image";
 import * as S from "./style";
 
 interface BookCardProps {
-  id: number;
   src: string;
   title: string;
   size: number;
   onClick?: React.MouseEventHandler<HTMLElement>;
 }
 
-export const BookCard = ({ id, src, title, size, onClick }: BookCardProps) => {
-  const handleClick = () => {
-    // console.log("id", id);
-  };
+export const BookCard = ({ src, title, size, onClick }: BookCardProps) => {
   return (
-    <S.BookCard onClick={handleClick} size={size}>
+    <S.BookCard onClick={onClick} size={size}>
       <S.ImageWrapper size={size}>
         <Image src={src} layout="fill" />
       </S.ImageWrapper>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { GetServerSideProps } from "next";
-import type { MouseEvent } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
 import type { BookDataInterface } from "../types/testType";
@@ -17,15 +16,8 @@ const Home = ({ books }: ServerSidePropsType) => {
   const router = useRouter();
   const { latestBooks, studyLatestBooks } = books;
 
-  const handleBookCardClick = (e: MouseEvent<HTMLElement>) => {
-    // TODO 책 클릭 시 상세 페이지로 이동!
-    // TODO 클릭 시 id 값을 얻어와야 한다.
-    // TODO BookCard에 onClick 콜백을 전달하면, 내부에서 id값을 알 수 없기 때문에 사용 불가.
-    // TODO List에서 클릭시 이동하는 방향으로 수정해야 할듯?
-  };
-
-  const handleBookCardListClick = (key: number, e: MouseEvent<HTMLElement>) => {
-    router.push(`/book/${key}`);
+  const handleBookCardClick = (id: number) => {
+    router.push(`/book/${id}`);
   };
 
   return (
@@ -33,16 +25,12 @@ const Home = ({ books }: ServerSidePropsType) => {
       <S.StyledSpan>가장 최근 추가된 책</S.StyledSpan>
       <S.StyledUl>
         {latestBooks.map((book) => (
-          <S.StyledList
-            key={book.id}
-            onClick={(e) => handleBookCardListClick(book.id, e)}
-          >
+          <S.StyledList key={book.id}>
             <BookCard
-              id={book.id}
               src={book.image}
               title=""
               size={10}
-              onClick={() => {}}
+              onClick={() => handleBookCardClick(book.id)}
             />
           </S.StyledList>
         ))}
@@ -51,16 +39,12 @@ const Home = ({ books }: ServerSidePropsType) => {
       <S.StyledSpan>가장 최근 스터디가 만들어진 책</S.StyledSpan>
       <S.StyledUl>
         {studyLatestBooks.map((book) => (
-          <S.StyledList
-            key={book.id}
-            onClick={(e) => handleBookCardListClick(book.id, e)}
-          >
+          <S.StyledList key={book.id}>
             <BookCard
-              id={book.id}
               src={book.image}
               title=""
               size={10}
-              onClick={() => {}}
+              onClick={() => handleBookCardClick(book.id)}
             />
           </S.StyledList>
         ))}
@@ -71,9 +55,9 @@ const Home = ({ books }: ServerSidePropsType) => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getServerSideProps: GetServerSideProps = async (/* context */) => {
   // TODO status 200이 아닐 때 Client로 전달할 prop과 API호출 실패시 전달할 prop 정의 필요
-
+  // TODO Router 사용이 필요할 경우 context 사용
   try {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_API_END_POINT}/books`
