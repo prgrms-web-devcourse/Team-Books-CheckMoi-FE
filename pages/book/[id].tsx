331a3e372c86b242e7c79f45e01652180c6bb52e
parkinhwa <inhwa6548@naver.com>
2022-08-04 23:11:10 +0900

    refactor: StudyCard map 로직 분리

--- pages/book/[id].tsx
+++ pages/book/[id].tsx
@@ -1,22 +1,16 @@
-import { Divider, Modal } from "@mui/material";
+import { Divider } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { BookDetail, StudyCard, Spacer } from "../../components";
+import { BookDetail } from "../../components";
 import type { BookType } from "../../types/bookType";
 import type { StudyType } from "../../types/studyType";
-import { StudyDetail } from "../../features/StudyDetail";
-import * as S from "../../styles/bookPageStyle";
+import { StudyCardList } from "../../features";
 
 const Book = () => {
   const router = useRouter();
   const [bookInfo, setBookinfo] = useState<BookType>({} as BookType);
   const [studies, setStudies] = useState<StudyType[]>([]);
-  const [selectedId, setSelectedId] = useState("");
   // TODO 쿠키에서 jwt 토큰 가져와서 user 정보 가져오기, 해당 데이터로 스터디원 인지 검증 로직 필요
-
-  const [open, setOpen] = useState(false);
-
-  const handleCloseClick = () => setOpen(false);
 
   useEffect(() => {
     const bookInfoFetch = async (id: string) => {
@@ -46,11 +40,6 @@ const Book = () => {
     }
   }, [router.query]);
 
-  const handleStudyClick = (id: string | undefined) => {
-    setSelectedId(id as string);
-    setOpen(!open);
-  };
-
   return (
     <div>
       <BookDetail
@@ -65,34 +54,7 @@ const Book = () => {
       />
 
       <Divider color="black" />
-      <S.StudyCardContainer>
-        {studies.map((study) => {
-          return (
-            <Spacer size={1}>
-              <StudyCard
-                key={study.id}
-                onClick={() => {
-                  handleStudyClick(study.id);
-                }}
-                study={study}
-                size={128}
-              />
-            </Spacer>
-          );
-        })}
-      </S.StudyCardContainer>
-
-      <Modal
-        open={open}
-        onClose={handleCloseClick}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-        disableEnforceFocus
-      >
-        <S.StyledBox>
-          <StudyDetail open={open} id={selectedId} />
-        </S.StyledBox>
-      </Modal>
+      <StudyCardList studies={studies} />
     </div>
   );
 };
