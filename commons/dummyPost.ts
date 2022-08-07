9bff21724e03e59369d38813aa2fb39260822c77
Minki Kim <alsrl0023@naver.com>
2022-08-07 16:49:46 +0900

    feat: 게시글 스타일 확인을 위한 DummyData 추가

---
+++ commons/dummyPost.ts
@@ -0,0 +1,154 @@
+import type { PostType } from "../types/postType";
+
+export const DummyPost: PostType[] = [
+  {
+    id: 1,
+    title: "Dummy Post1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 2,
+    title: "Dummy Post2",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 3,
+    title: "Dummy Post3",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 4,
+    title: "Dummy Post4",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 5,
+    title: "Dummy Post5",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 6,
+    title: "Dummy Post6",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 7,
+    title: "Dummy Post7",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 8,
+    title: "Dummy Post8",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 9,
+    title: "Dummy Post9",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+  {
+    id: 10,
+    title: "Dummy Post10",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, nemo harum sapiente molestias non iste facilis rerum numquam, ducimus unde ut. Reprehenderit in expedita nostrum aut porro libero, ab debitis.",
+    createdAt: "2020/08/07",
+    comments: 10,
+    size: 20,
+    user: {
+      userId: "DummyID",
+      name: "Dummy",
+      email: "DummyEmail",
+      img: "https://picsum.photos/200",
+    },
+  },
+];
