2f1a583e00ce505943ff424d715bc0c86cc492ff
feel0321 <dishate@naver.com>
2022-08-09 22:09:55 +0900

    feat: pagination 추가

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,18 +1,29 @@
-import { Box } from "@mui/system";
+import { Pagination, Box } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import type { ChangeEvent } from "react";
 import { getNaverBooks, registerBook } from "../../apis";
 import { BookCard } from "../../components";
 import type { NaverBookResponseType } from "../../types/bookType";
 
 const SearchPage = () => {
   const router = useRouter();
+  const { word, page } = router.query;
 
   const [loading, setLoading] = useState(true);
   const [searchedInfo, setSearchedInfo] = useState({} as NaverBookResponseType);
 
+  const handlePaginationChange = (_: ChangeEvent<unknown>, two: number) => {
+    router.push({
+      pathname: "/search",
+      query: {
+        word,
+        page: two,
+      },
+    });
+  };
+
   useEffect(() => {
-    const { word, page } = router.query;
     if (!word) return;
 
     const searchNaverBook = async () => {
@@ -30,33 +41,44 @@ const SearchPage = () => {
   return loading ? (
     "로딩 중"
   ) : (
-    <Box
-      sx={{
-        display: "flex",
-        gap: "1rem",
-        flexWrap: "wrap",
-      }}
-    >
-      {searchedInfo.items.length
-        ? searchedInfo.items.map((book) => (
-            <BookCard
-              key={book.isbn}
-              src={book.image}
-              title={book.title}
-              size={10}
-              onClick={() => {
-                registerBook(
-                  book,
-                  process.env.NEXT_PUBLIC_FAKE_TOKEN as string
-                ).then((response) => {
-                  console.log(response);
-                  router.push(`/book/${response}`);
-                });
-              }}
-            />
-          ))
-        : "검색 결과가 없습니다"}
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "1rem",
+          flexWrap: "wrap",
+          marginBottom: "2rem",
+        }}
+      >
+        {searchedInfo.items.length
+          ? searchedInfo.items.map((book) => (
+              <BookCard
+                key={book.isbn}
+                src={book.image}
+                title={book.title}
+                size={10}
+                onClick={() => {
+                  registerBook(
+                    book,
+                    process.env.NEXT_PUBLIC_FAKE_TOKEN as string
+                  ).then((response) => {
+                    console.log(response);
+                    router.push(`/book/${response}`);
+                  });
+                }}
+              />
+            ))
+          : "검색 결과가 없습니다"}
+      </Box>
+      <Pagination
+        count={Math.ceil(searchedInfo.total / 10)}
+        variant="outlined"
+        shape="rounded"
+        color="primary"
+        page={Number(router.query?.page)}
+        onChange={handlePaginationChange}
+      />
+    </>
   );
 };
 
