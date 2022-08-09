1ef16327c39593f6396568feebf6c4413e2bf126
blacktoast <dbstjd100@naver.com>
2022-08-09 15:41:32 +0900

    feat: 무한스크롤 훅 구현

---
+++ hooks/useInView.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef, useState } from "react";
+
+export const useInView = () => {
+  const [inView, setInView] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const options = {
+      threshold: 1.0,
+    };
+    let observer: IntersectionObserver;
+
+    if (ref.current) {
+      observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) setInView(true);
+        if (!entries[0].isIntersecting) setInView(false);
+      }, options);
+      observer.observe(ref.current);
+    }
+    return () => observer && observer.disconnect();
+  }, [ref]);
+
+  return [ref, inView];
+};
