a4020a339d8ed818b0b6de4561f0b4af95b70ac4
blacktoast <dbstjd100@naver.com>
2022-08-11 17:46:49 +0900

    feat: 스터디원 이면 스터디 가입 아니면 스터디 페이지로 이동 로직 구현

--- features/StudyDetail/StudyDetail.tsx
+++ features/StudyDetail/StudyDetail.tsx
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import { Divider } from "@mui/material";
+import { useRouter } from "next/router";
 import type { StudyType } from "../../types/studyType";
 import { StudyContent } from "../../components/StudyContent";
 import { StudyDetailCard } from "../../components/StudyDetailCard";
-import { getStudyDetailInfo } from "../../apis/study";
-import { UserType } from "../../types/userType";
-import { BookType } from "../../types/bookType";
+import { getStudyDetailInfo, joinStudy } from "../../apis/study";
+import type { TopbarUserType, UserType } from "../../types/userType";
+import type { BookType } from "../../types/bookType";
+import { useUserContext } from "../../hooks/useUserContext";
+import { useOurSnackbar } from "../../hooks/useOurSnackbar";
+import { LoginRequestModal } from "../LoginRequestModal";
 
 interface StudyDetailProps {
   id: string;
@@ -24,13 +28,36 @@ export const StudyDetail = ({
   open = false,
   isPage = false,
 }: StudyDetailProps) => {
+  const { user } = useUserContext();
+  const [isMember, setIsMember] = useState(false);
+  const { renderSnackbar } = useOurSnackbar();
+  const [openModal, setOpenModal] = useState(false);
+  const router = useRouter();
   const [studyInfo, setStudyInfo] = useState({
     study: {},
   } as Test);
 
+  const enterStudy = async (
+    studyId: string,
+    inputUser: TopbarUserType | null
+  ) => {
+    if (!inputUser) {
+      setOpenModal(true);
+      return;
+    }
+
+    try {
+      const [_, token] = document.cookie.split("token=");
+      await joinStudy(studyId, token);
+    } catch (error) {
+      renderSnackbar("스터디 참여에 실패 했습니다", "error");
+    }
+  };
+
   useEffect(() => {
     const studyInfoFetch = async (studyId: string) => {
       const { study, members, book } = await getStudyDetailInfo(studyId);
+      if (members.filter((member) => member.id === user?.id)) setIsMember(true);
 
       setStudyInfo({
         study,
@@ -41,16 +68,41 @@ export const StudyDetail = ({
     if (open) studyInfoFetch(id);
   }, [open]);
 
+  const handleStudyJoinClick = async () => {
+    if (!user) setOpenModal(true);
+    else enterStudy(id, user);
+  };
+
+  const handleGoStudyClick = () => {
+    router.push(`/study/${id}`);
+  };
+
+  const handleOnCloseClick = () => {
+    setOpenModal(false);
+  };
+
   return (
     <div>
       <StudyDetailCard study={studyInfo.study} members={studyInfo.members} />
       <Divider color="grey" />
       <StudyContent
         description={studyInfo.study.description}
-        isMember={false}
-        onClick={() => {}}
+        isMember={isMember}
+        joinOnClick={handleStudyJoinClick}
+        goStudyOnClick={handleGoStudyClick}
         height={isPage ? "40vh" : 10}
       />
+      <LoginRequestModal.Container
+        open={openModal}
+        onClose={handleOnCloseClick}
+      >
+        <LoginRequestModal.Title>
+          스터디 참여하기 위해서 로그인이 필요합니다
+        </LoginRequestModal.Title>
+        <LoginRequestModal.Content>
+          로그인을 하시겠습니까?
+        </LoginRequestModal.Content>
+      </LoginRequestModal.Container>
     </div>
   );
 };
