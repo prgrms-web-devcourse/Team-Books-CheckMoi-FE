272d21d892f1e9ed1a99a91991870556142fd40d
feel0321 <dishate@naver.com>
2022-08-11 15:16:31 +0900

    feat: BookCardSkeleton 추가, Booktitle height 고정

---
+++ components/BookCard/BookCardSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "@mui/material";
+import * as S from "./style";
+
+interface BookCardSkeletonProps {
+  size: number;
+}
+
+export const BookCardSkeleton = ({ size }: BookCardSkeletonProps) => {
+  return (
+    <>
+      <Skeleton
+        variant="rectangular"
+        width={`${size}rem`}
+        height={`${size * 1.25}rem`}
+      />
+      <S.BookTitle />
+    </>
+  );
+};
