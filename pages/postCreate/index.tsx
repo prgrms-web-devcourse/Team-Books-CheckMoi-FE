098f5cbbcd9c55282d38b92f953bfb8164d9c06e
parkinhwa <inhwa6548@naver.com>
2022-08-11 17:44:47 +0900

    feat: PostCreate 페이지

--- pages/postCreate/index.tsx
+++ pages/postCreate/index.tsx
@@ -1,7 +1,17 @@
 import { PostForm } from "../../features";
 
 const PostCreatePage = () => {
-  return <PostForm selectValue="NOTICE" title="" content="" />;
+  // TODO selectValue와 studyId 전달받아서 넣어주기
+  return (
+    <PostForm
+      state="POST"
+      postId={-1}
+      selectValue="NOTICE"
+      title=""
+      content=""
+      studyId={1}
+    />
+  );
 };
 
 export default PostCreatePage;
